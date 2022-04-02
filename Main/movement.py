10e52db40a587a19578541df0cd442bfe149bc51
Yashu-the-great <yashuff1@gmail.com>
2022-04-02 22:52:03 +0530

    movement controls (for future ;))

---
+++ Main/movement.py
@@ -0,0 +1,11 @@
+def move_forward():
+    print("Moving Forward")
+
+def move_backward():
+    print("Moving Backward")
+
+def move_left():
+    print("Moving Left")
+
+def move_right():
+    print("Moving Right")
