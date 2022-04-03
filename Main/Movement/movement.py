51a6283b6fef33467a4ba2671fa57eb4cf1d5cda
Yashu-the-great <yashuff1@gmail.com>
2022-04-03 11:51:34 +0530

    movement functions in their respected file

---
+++ Main/Movement/movement.py
@@ -0,0 +1,17 @@
+def move_forward():
+    print("Moving Forward")
+
+def move_backward():
+    print("Moving Backward")
+
+def move_left():
+    print("Moving Left")
+
+def move_right():
+    print("Moving Right")
+
+def no_vertical_movement():
+    print("No Vertical Movement")
+
+def no_horizontal_movement():
+    print("No Horizontal Movement")
